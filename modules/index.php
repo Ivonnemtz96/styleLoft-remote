b61a7eaceb09b448f335f799cf93a9b05b3da392
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-28 09:41:27 -0700

    Diseño de index

---
+++ modules/index.php
@@ -0,0 +1,307 @@
+<!-- Bnner Section -->
+<section class="banner-section style-two">
+    <div class="swiper-container banner-slider">
+        <div class="swiper-wrapper">
+            <!-- Slide Item -->
+            <div class="swiper-slide" style="background-image: url(assets/images/main-slider/image-3.jpg);">
+                <div class="content-outer">
+                    <div class="content-box text-center justify-content-center">
+                        <div class="inner">
+                            <h4>Welcome to Quality Tayler</h4>
+                            <h1>Create your <br> own Personal Style</h1>
+                            <div class="link-box">
+                                <a href="services.html" class="theme-btn btn-style-one"><span>discover more</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Slide Item -->
+            <div class="swiper-slide" style="background-image: url(assets/images/main-slider/image-4.jpg);">
+                <div class="content-outer">
+                    <div class="content-box text-center justify-content-center">
+                        <div class="inner">
+                            <h4>Welcome to Quality Tayler</h4>
+                            <h1>our Personal <br> Quality tayler</h1>
+                            <div class="link-box">
+                                <a href="services.html" class="theme-btn btn-style-one"><span>discover more</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="banner-slider-nav">
+        <div class="banner-slider-control banner-slider-button-prev"><span><i class="fas fa-arrow-right"></i></span>
+        </div>
+        <div class="banner-slider-control banner-slider-button-next"><span><i class="fas fa-arrow-right"></i></span>
+        </div>
+    </div>
+</section>
+<!-- End Bnner Section -->
+
+<!-- Features section -->
+<section class="features-section">
+    <div class="auto-container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="feature-block">
+                    <div class="feature-block_image"><img src="assets/images/resource/image-14.jpg" alt=""></div>
+                    <h4 class="feature-block_title">True Quality</h4>
+                    <div class="feature-block_hover-content">
+                        <h4 class="feature-block_title">True Quality</h4>
+                        <div class="feature-block_text">Lorem ipsum is not simply <br> free text in market</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="feature-block">
+                    <div class="feature-block_image"><img src="assets/images/resource/image-27.jpg" alt=""></div>
+                    <h4 class="feature-block_title">Handcrafted Suits</h4>
+                    <div class="feature-block_hover-content">
+                        <h4 class="feature-block_title">Handcrafted Suits</h4>
+                        <div class="feature-block_text">Lorem ipsum is not simply <br> free text in market</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="feature-block">
+                    <div class="feature-block_image"><img src="assets/images/resource/image-15.jpg" alt=""></div>
+                    <h4 class="feature-block_title">Kevin in Media</h4>
+                    <div class="feature-block_hover-content">
+                        <h4 class="feature-block_title">Kevin in Media</h4>
+                        <div class="feature-block_text">Lorem ipsum is not simply <br> free text in market</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Introduction -->
+<section class="introduction-section">
+    <div class="auto-container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="introduction-block_image">
+                    <div class="img_hover_3">
+                        <img src="assets/images/resource/image-16.jpg" alt="">
+                    </div>
+                    <div class="introduction-block_content" data-parallax='{"y": 30}'>
+                        <div class="introduction-block_text-two">Trusted and Professional <br> Taylers in Town</div>
+                        <a class="introduction-block_link-btn-two" href="about.html">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="introduction-block">
+                    <div class="sec-title-dec"><img src="assets/images/shape/shape-1.png" alt=""></div>
+                    <div class="sub-title">Our Introduction </div>
+                    <div class="sec-title">Our Taylers Ready to <br> nStich your Clothes</div>
+                    <div class="introduction-block_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+                        deiusmod tempor incididunt ut labore et dolore magna aliqua. Donec scelerisque dolor id nunc
+                        dictum.</div>
+                    <ul class="introduction-block_list">
+                        <li><i class="icon"><img src="assets/images/shape/shape-2.png" alt=""></i>Suspe ndisse suscipit
+                            sagittis leo.</li>
+                        <li><i class="icon"><img src="assets/images/shape/shape-2.png" alt=""></i>Lorem Ipsum generators
+                            on the tend to repeat.</li>
+                    </ul>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="introduction-block_contact-info">
+                                <div class="introduction-block_phone-title">Call Anytime</div>
+                                <div class="introduction-block-phone"><a href="tel:+1-(246)3330089">+ 1- (246)
+                                        333-0089</a></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="introduction-block_link-btn"><a href="about.html"
+                                    class="theme-btn btn-style-one">discover more</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Services Offer -->
+<section class="service-offer-section">
+    <div class="auto-container">
+        <div class="text-center">
+            <div class="sec-title-dec"><img src="assets/images/shape/shape-1.png" alt=""></div>
+            <div class="sub-title">Checkout our services</div>
+            <div class="sec-title">Services We Offer</div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="service-block-three">
+                    <div class="service-block-three_border_top"></div>
+                    <div class="service-block-three_border_left"></div>
+                    <div class="service-block-three_border_right"></div>
+                    <div class="service-block-three_border_bottom"></div>
+                    <div class="service-block-three_image img_hover_2"><img src="assets/images/resource/image-3.jpg"
+                            alt=""></div>
+                    <h4 class="service-block-three_title">Suits & Shirts</h4>
+                    <div class="service-block-three_text">Donec suscipit ipsum quis est mentum, quis sollicitudin libero
+                        tempus.</div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="service-block-three">
+                    <div class="service-block-three_border_top"></div>
+                    <div class="service-block-three_border_left"></div>
+                    <div class="service-block-three_border_right"></div>
+                    <div class="service-block-three_border_bottom"></div>
+                    <div class="service-block-three_image img_hover_2"><img src="assets/images/resource/image-4.jpg"
+                            alt=""></div>
+                    <h4 class="service-block-three_title">Stylish Clothing</h4>
+                    <div class="service-block-three_text">Donec suscipit ipsum quis est mentum, quis sollicitudin libero
+                        tempus.</div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="service-block-three">
+                    <div class="service-block-three_border_top"></div>
+                    <div class="service-block-three_border_left"></div>
+                    <div class="service-block-three_border_right"></div>
+                    <div class="service-block-three_border_bottom"></div>
+                    <div class="service-block-three_image img_hover_2"><img src="assets/images/resource/image-5.jpg"
+                            alt=""></div>
+                    <h4 class="service-block-three_title">Wedding Dresses</h4>
+                    <div class="service-block-three_text">Donec suscipit ipsum quis est mentum, quis sollicitudin libero
+                        tempus.</div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="service-block-three">
+                    <div class="service-block-three_border_top"></div>
+                    <div class="service-block-three_border_left"></div>
+                    <div class="service-block-three_border_right"></div>
+                    <div class="service-block-three_border_bottom"></div>
+                    <div class="service-block-three_image img_hover_2"><img src="assets/images/resource/image-9.png"
+                            alt=""></div>
+                    <h4 class="service-block-three_title">Custom Work</h4>
+                    <div class="service-block-three_text">Donec suscipit ipsum quis est mentum, quis sollicitudin libero
+                        tempus.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Video -->
+<section class="video-section">
+    <div class="video-section_bg" data-parallax='{"y": 30}'
+        style="background-image: url(assets/images/background/bg-3.jpg)"></div>
+    <div class="auto-container">
+        <div class="row">
+            <div class="video-btn"><a href="https://www.youtube.com/watch?v=nfP5N9Yc72A&amp;t=28s"
+                    class="overlay-link play-now ripple" data-fancybox="gallery" data-caption=""><i
+                        class="fas fa-play"></i></a></div>
+            <h4 class="video-block_title">You Can Depend on Our <br> Taylers to Get a Quality Dresses</h4>
+        </div>
+    </div>
+</section>
+
+<!-- Gallery -->
+<section class="gallery-section">
+    <div class="auto-continer">
+        <div class="row row-3">
+            <div class="col-lg-3 col-md-6">
+                <div class="gallery-block gallery-overlay">
+                    <div class="gallery-block_image"><img src="assets/images/resource/image-17.jpg" alt=""></div>
+                    <div class="overlay-box">
+                        <div class="overlay-inner">
+                            <div class="content">
+                                <a href="assets/images/resource/image-17.jpg" class="lightbox-image link"
+                                    data-fancybox="gallery"><span class="icon fal fa-plus"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="gallery-block gallery-overlay">
+                    <div class="gallery-block_image"><img src="assets/images/resource/image-18.jpg" alt=""></div>
+                    <div class="overlay-box">
+                        <div class="overlay-inner">
+                            <div class="content">
+                                <a href="assets/images/resource/image-18.jpg" class="lightbox-image link"
+                                    data-fancybox="gallery"><span class="icon fal fa-plus"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="gallery-block gallery-overlay">
+                    <div class="gallery-block_image"><img src="assets/images/resource/image-19.jpg" alt=""></div>
+                    <div class="overlay-box">
+                        <div class="overlay-inner">
+                            <div class="content">
+                                <a href="assets/images/resource/image-19.jpg" class="lightbox-image link"
+                                    data-fancybox="gallery"><span class="icon fal fa-plus"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="gallery-block gallery-overlay">
+                    <div class="gallery-block_image"><img src="assets/images/resource/image-20.jpg" alt=""></div>
+                    <div class="overlay-box">
+                        <div class="overlay-inner">
+                            <div class="content">
+                                <a href="assets/images/resource/image-20.jpg" class="lightbox-image link"
+                                    data-fancybox="gallery"><span class="icon fal fa-plus"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Our testimonials -->
+<section class="our-testimonials-section-two">
+    <div class="auto-container">
+        <div class="text-center">
+            <div class="sec-title-dec"><img src="assets/images/shape/shape-1.png" alt=""></div>
+            <div class="sub-title">Our testimonials </div>
+            <div class="sec-title">What They’re Saying</div>
+        </div>
+        <div class="theme_carousel owl-theme owl-carousel owl-style-one"
+            data-options='{"loop": true, "center": false, "margin": 0, "autoheight":true, "lazyload":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 6000, "smartSpeed": 1000, "responsive":{ "0" :{ "items": "1" }, "480" :{ "items" : "1" }, "600" :{ "items" : "2" }, "768" :{ "items" : "2" } , "992":{ "items" : "3" }, "1200":{ "items" : "3" }}}'>
+            <div class="testimonial-block-two">
+                <div class="testimonial-block-two_image"><img src="assets/images/resource/image-6.png" alt=""></div>
+                <div class="testimonial-block-two_text">I was very impresed by the company services lorem ipsum is
+                    simply free text available used by copytyping refreshing. Neque porro est qui dolorem ipsum quia.
+                </div>
+                <div class="testimonial-block-two_name">Mike Hardson</div>
+                <div class="testimonial-block-two_dasignation">Customer</div>
+            </div>
+
+            <div class="testimonial-block-two">
+                <div class="testimonial-block-two_image"><img src="assets/images/resource/image-7.png" alt=""></div>
+                <div class="testimonial-block-two_text">I was very impresed by the company services lorem ipsum is
+                    simply free text available used by copytyping refreshing. Neque porro est qui dolorem ipsum quia.
+                </div>
+                <div class="testimonial-block-two_name">Sarah Albert</div>
+                <div class="testimonial-block-two_dasignation">Customer</div>
+            </div>
+
+            <div class="testimonial-block-two">
+                <div class="testimonial-block-two_image"><img src="assets/images/resource/image-8.png" alt=""></div>
+                <div class="testimonial-block-two_text">I was very impresed by the company services lorem ipsum is
+                    simply free text available used by copytyping refreshing. Neque porro est qui dolorem ipsum quia.
+                </div>
+                <div class="testimonial-block-two_name">Kevin Martin</div>
+                <div class="testimonial-block-two_dasignation">Customer</div>
+            </div>
+        </div>
+    </div>
+</section>
